e9433ce94a84088b74741e545d69e818418770d3
rwxce <148581386+rwxce@users.noreply.github.com>
2025-03-25 18:33:12 +0100

    fix circular references

--- WebApplicationTgtNotes/App_Start/WebApiConfig.cs
+++ WebApplicationTgtNotes/App_Start/WebApiConfig.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web.Http;
+﻿using System.Web.Http;
 
 namespace WebApplicationTgtNotes
 {
@@ -19,6 +16,9 @@ namespace WebApplicationTgtNotes
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling =
+                Newtonsoft.Json.ReferenceLoopHandling.Ignore;
         }
     }
 }
