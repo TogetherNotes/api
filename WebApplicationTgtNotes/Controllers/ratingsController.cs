e20c008950e23a52117967e4ea987b5647b699c8
rwxce <148581386+rwxce@users.noreply.github.com>
2025-03-31 15:28:18 +0200

    fix db model prop

--- WebApplicationTgtNotes/Controllers/ratingsController.cs
+++ WebApplicationTgtNotes/Controllers/ratingsController.cs
@@ -24,7 +24,7 @@ namespace WebApplicationTgtNotes.Controllers
                 .Select(r => new
                 {
                     r.id,
-                    r.rating_num,
+                    r.rating1,
                     r.artist_id,
                     r.space_id
                 })
@@ -46,7 +46,7 @@ namespace WebApplicationTgtNotes.Controllers
                 .Select(r => new
                 {
                     r.id,
-                    r.rating_num,
+                    r.rating1,
                     r.artist_id,
                     r.space_id
                 })
